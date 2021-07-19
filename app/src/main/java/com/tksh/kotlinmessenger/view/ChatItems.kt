ee2e26ec2dae8caa301aee388ab4c47a9070177e
Tanishq Kashyap <tanishqa89@gmail.com>
2021-07-19 16:01:00 +0530

    changed the send layout to right and receive layout to left

--- app/src/main/java/com/tksh/kotlinmessenger/view/ChatItems.kt
+++ app/src/main/java/com/tksh/kotlinmessenger/view/ChatItems.kt
@@ -12,24 +12,24 @@ import kotlinx.android.synthetic.main.chat_to_row.view.*
 
 class ChatFromItem(val user: User,val chatMessage: ChatMessage) : Item<GroupieViewHolder>(){
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-        viewHolder.itemView.text_from_chatLog.text = chatMessage.text
-        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.image_from_chatLog)
-    }
-
-    override fun getLayout(): Int {
-        return R.layout.chat_from_row
-    }
-
-}
-class ChatToItem( private val chatMessage: ChatMessage, val user: User) : Item<GroupieViewHolder>(){
-    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         viewHolder.itemView.text_to_chatLog.text = chatMessage.text
-
         Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.image_to_chatLog)
     }
 
     override fun getLayout(): Int {
         return R.layout.chat_to_row
+    }
+
+}
+class ChatToItem( private val chatMessage: ChatMessage, val user: User) : Item<GroupieViewHolder>(){
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.itemView.text_from_chatLog.text = chatMessage.text
+
+        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.image_from_chatLog)
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.chat_from_row
     }
 
 }
